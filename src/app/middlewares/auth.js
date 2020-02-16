f6817b2922e4b3213c7801e923eec3df6c2fa3b0
fbc623 <fbc623gmail.com>
2020-02-16 12:05:43 -0300

    criado rotas para entregadores

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -6,8 +6,7 @@ import authConfig from '../../config/auth';
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader)
-    return res.status(401).json({ error: 'Token not provided' });
+  if (!authHeader) return res.status(401).json({ error: 'Token not provided' });
 
   const [, token] = authHeader.split(' ');
 
