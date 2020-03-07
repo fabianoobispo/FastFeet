f0cf3003eed02595a7468054dd932bdb2fc07b3a
fbc623 <fbc623gmail.com>
2020-03-07 17:23:10 -0300

    criando inputs da pagina inicial

--- web/src/pages/SingIn/index.js
+++ web/src/pages/SingIn/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
+import { Form } from '@unform/web';
+
 import logo from '~/assets/logo.svg';
+import { SimpleInput } from '~/components/Form';
 
 export default function SingIn() {
   return (
     <>
       <img src={logo} alt="FastFeet" />
+
+      <Form>
+        <SimpleInput
+          name="email"
+          label="SEU E-MAIL"
+          type="email"
+          placeholder="exemplo@email.com"
+        />
+        <SimpleInput
+          name="password"
+          label="SUA SENHA"
+          type="password"
+          placeholder="*************"
+        />
+      </Form>
     </>
   );
 }
